e39b422b55b2c67b924a615d95ddfa26dbe6e441
Volkman Adam - IT2 <volkmanadam215@student.voskh.cz>
2022-11-28 14:05:21 +0100

    feat: header, bootstrap, typewriter

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var current = 0;
+
+function typeWrite(id, text, speed, callback) {
+  if (current < text.length) {
+    document.getElementById(id).innerHTML += text.charAt(current);
+    current++;
+    setTimeout(()=> typeWrite(id, text, speed, callback), speed);
+  } else {
+    callback();
+  }
+}
+
+typeWrite("name", "Adam Volkman", 75, () => {
+    current = 0;
+    typeWrite("nick", "maku", 50, () => {});
+});
